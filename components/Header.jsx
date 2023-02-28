d9db37257878d97d98246f6d6e723890f9af31d9
shawn2776 <shawn.harrington2776@gmail.com>
2023-02-27 16:36:43 -0800

    init push

---
+++ components/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex justify-between shadow-2xl min-h-[50px] items-center px-40 text-xl bg-gray-900 text-white ">
+      <div>
+        <span></span>
+      </div>
+      <div>
+        <span className="mr-5 hover:text-orange-600 cursor-pointer">Home</span>
+        <span className="mr-5 hover:text-orange-600 cursor-pointer">
+          Projects
+        </span>
+        <span className="mr-5 hover:text-orange-600 cursor-pointer">
+          Skills
+        </span>
+        <span className="mr-5 hover:text-orange-600 cursor-pointer">About</span>
+        <span className="mr-5 hover:text-orange-600 cursor-pointer">
+          Contact
+        </span>
+        <span className="hover:text-orange-600 cursor-pointer">Resume</span>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import { Inter } from "@next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -14,110 +13,38 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+      <main className="relative min-h-screen pl-80 bg-gray-900">
+        <div className="absolute bottom-0 left-40 mb-60">
+          <h1 className="text-8xl uppercase tracking-tighter shimmer">
+            Shawn
+            <br />
+            Harrington
+          </h1>
+          <h2 className="text-6xl uppercase tracking-tighter">
+            <span className="text-orange-600">Full Stack</span>{" "}
+            <span className="text-orange-400">Software</span>{" "}
+            <span className="text-orange-600">Engineer</span>
+          </h2>
+          <div className="mt-5">
+            <button className="rounded text-2xl px-8 py-3 bg-black text-white hover:bg-white hover:text-black hover:border-black hover:border">
+              <strong>About Me</strong>
+            </button>
+            <button className="ml-3 rounded text-2xl px-8 py-3 bg-black text-white hover:bg-white hover:text-black hover:border-black hover:border">
+              <strong>Hire Me</strong>
+            </button>
           </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
         </div>
       </main>
+      <div className="bg-orange-600 text-gray-300 pl-40">
+        <p className="text-4xl py-10 max-w-3xl">
+          I am an <strong className="text-white">enthusiastic developer</strong>{" "}
+          with a strong addiction to discovering{" "}
+          <strong className="text-white">new coding perspectives.</strong>
+        </p>
+      </div>
+      <div className="pl-40 pt-20 text-3xl bg-gray-900 text-white">
+        <h2 className="font-bold">PROJECTS</h2>
+      </div>
     </>
-  )
+  );
 }
